fa17b991ac7102a8abed7e5ce7e5151a74247286
ljc1035 <dayzplayerwasd@gmail.com>
2020-04-24 00:24:46 -0400

    Fixed input handler to actually work (hopefully)

--- ECE583_Final_Project.srcs/sources_1/new/input_handler.v
+++ ECE583_Final_Project.srcs/sources_1/new/input_handler.v
@@ -1,4 +1,4 @@
-module inputHandler
+module input_handler
 #(
 parameter DATA_BITS = 8,
 parameter INPUT_BITS = 5+16+8
@@ -15,16 +15,14 @@ output reg ready_signal_out
     wire [INPUT_BITS-1:0] cur_state = {BTN[4:0], SW[15:0], JA[7:0]};
     reg [INPUT_BITS-1:0] prev_state;
 
-    // Send data
+    // Determine whether data needs to be sent, and determines what data to send
+    integer i;
     always @ ( posedge clk_in ) begin
         prev_state <= cur_state;
         ready_signal_out <= (prev_state ^ cur_state) ? 1: 0;
-        genvar i;
-        generate
-            for (i=0; i<INPUT_BITS; i=i+1) begin: loop
-                if (cur_state[i] ^ prev_state[i])
-                    data_out <= (cur_state[i]) ? (i*2) : ((i*2)+1);
-            end
-        endgenerate
+        for (i=0; i<INPUT_BITS; i=i+1) begin
+            if (cur_state[i] ^ prev_state[i])
+                data_out <= (cur_state[i]) ? (i*2) : ((i*2)+1);
+        end
     end
-endmodule // inputHandler
+endmodule // input_handler

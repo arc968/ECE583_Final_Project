f0af690eb7c0237cb9aae45bb7aa19caf3ca11d5
otter968 <andrewcourtemanche968@gmail.com>
2020-04-30 15:05:02 -0400

    tetris refactor init

---
+++ Arduino/tetris_remake/tetris.h
@@ -0,0 +1,80 @@
+#define COMMAND_BUFFER_SELECT 0x01
+#define COMMAND_GPU_LOAD_MASK 0x03
+#define COMMAND_GPU_LOAD_COL 0x04
+#define COMMAND_GPU_DRAW 0x05
+#define COMMAND_GPU_BRIGHTNESS 0x06
+
+static uint8_t last_buffer = 0;
+
+//select=write, select+1=displayed, select+2=cleared
+void fpga_select_buffer(HardwareSerial &port, uint8_t select){
+  Serial3.write(COMMAND_BUFFER_SELECT);
+  Serial3.write(select);
+}
+
+void fpga_next_buffer(HardwareSerial &port){
+  last_buffer = (last_buffer == 2) ? 0 : last_buffer + 1;
+  fpga_select_buffer(port, last_buffer);
+}
+
+void gpu_load_mask(HardwareSerial &port, uint8_t select, uint8_t mask[8]){
+  Serial3.write(COMMAND_GPU_LOAD_MASK);
+  Serial3.write(select);
+  for(int i=0; i<8; i++){
+    Serial3.write(mask[i]);
+  }
+}
+
+void gpu_load_color(HardwareSerial &port, uint8_t select, uint8_t red, uint8_t green, uint8_t blue){
+  Serial3.write(COMMAND_GPU_LOAD_COL);
+  Serial3.write(select);
+  Serial3.write(red);
+  Serial3.write(green);
+  Serial3.write(blue);
+}
+
+void gpu_draw(HardwareSerial &port, uint8_t select, uint8_t x, uint8_t y){
+  Serial3.write(COMMAND_GPU_DRAW);
+  Serial3.write(select);
+  Serial3.write(x);
+  Serial3.write(y);
+}
+
+void gpu_brightness(HardwareSerial &port, uint8_t val){
+  Serial3.write(COMMAND_GPU_BRIGHTNESS);
+  Serial3.write(val);
+}
+
+void gpu_draw_shape(HardwareSerial &port, uint8_t x, uint8_t y, uint8_t * mask, uint8_t color, uint32_t * COLOR_LIST){
+  gpu_load_mask(port, 0, mask);
+  gpu_load_color(port, 0, (COLOR_LIST[color]>>16)&0xFF, (COLOR_LIST[color]>>8)&0xFF, (COLOR_LIST[color]>>0)&0xFF);
+  gpu_draw(port, 0, x, y);
+}
+
+void gpu_draw_board(HardwareSerial &port, uint8_t * board, uint8_t * colors, uint32_t * COLOR_LIST){
+  for(int y=0; y<4; y++){
+    for(int x=0; x<4; x++){
+      uint64_t mask[16];
+      for(int yy=0; yy<8; yy++){
+        for(int xx=0; xx<8; xx++){
+          for(int i=0; i<16; i++){
+            if(board[(xx+(x*8))+((yy+(y*8))*32)] == 1 && colors[(xx+(x*8))+((yy+(y*8))*32)] == i){
+              mask[i] = (mask[i] << 1) | 0x1;
+            }else{
+              mask[i] = (mask[i] << 1) | 0x0;
+            }
+          }
+        }
+      }
+      for(int i=0; i<16; i++){
+        uint8_t temp_mask[8];
+        for(int ii=0; ii<8; ii++){
+          temp_mask[ii] = mask[i]>>((7-ii)*8);
+        }
+        gpu_load_mask(port, i, temp_mask);
+        gpu_load_color(port, i, (COLOR_LIST[i]>>16)&0xFF, (COLOR_LIST[i]>>8)&0xFF, (COLOR_LIST[i]>>0)&0xFF);
+        gpu_draw(port, i, x*8, y*8);
+      }
+    }
+  }
+}
---
+++ Arduino/tetris_remake/tetris_remake.ino
@@ -0,0 +1,110 @@
+#include "tetris.h"
+
+#define PANEL_WIDTH 32
+#define PANEL_HEIGHT 32
+#define PANEL_SIZE PANEL_WIDTH*PANEL_HEIGHT
+#define BOARD_WIDTH 10
+#define BOARD_HEIGHT 20
+#define BOARD_X 0
+#define BOARD_Y PANEL_HEIGHT-BOARD_HEIGHT
+
+typedef struct{
+  const uint8_t color[3];
+  const uint16_t masks[4];
+} Shape;
+
+const Shape SHAPE_I = {
+  {0x00, 0xFF, 0xFF}, //cyan
+  {0b0000111100000000,
+   0b0010001000100010,
+   0b0000000011110000,
+   0b0100010001000100}
+};
+
+const Shape SHAPE_J = {
+  {0x00, 0x00, 0xFF}, //blue
+  {0b1000111000000000,
+   0b0110010001000000,
+   0b0000111000100000,
+   0b0100010011000000}
+};
+
+const Shape SHAPE_L = {
+  {0xFF, 0x7F, 0x00}, //orange
+  {0b0010111000000000,
+   0b0100010001100000,
+   0b0000111010000000,
+   0b1100010001000000}
+};
+
+const Shape SHAPE_O = {
+  {0xFF, 0xFF, 0x00}, //yellow
+  {0b1100110000000000,
+   0b1100110000000000,
+   0b1100110000000000,
+   0b1100110000000000}
+};
+                                   
+const Shape SHAPE_S = {
+  {0x00, 0xFF, 0x00}, //green
+  {0b0110110000000000,
+   0b0100011000100000,
+   0b0000011011000000,
+   0b1000110001000000}
+};
+
+const Shape SHAPE_T = {
+  {0xFF, 0x00, 0xFF}, //purple
+  {0b0100111000000000,
+   0b0100011001000000,
+   0b0000111001000000,
+   0b0100110001000000}
+};
+
+const Shape SHAPE_Z = {
+  {0xFF, 0x00, 0x00}, //red
+  {0b1100011000000000,
+   0b0010011001000000,
+   0b0000110001100000,
+   0b0100110010000000}
+};
+
+const Shape SHAPES[7] = {SHAPE_I, SHAPE_J, SHAPE_L, SHAPE_O, SHAPE_S, SHAPE_T, SHAPE_Z};
+
+void setup() {
+  noInterrupts();           // disable all interrupts
+  TCCR5A = 0;               //init clear
+  TCCR5B = 0;               //init clear
+  TCNT5  = 0;               //init clear
+  OCR5A = 1042;             // compare match register 16MHz/256/60Hz
+  TCCR5B |= (1 << WGM12);   // CTC mode
+  TCCR5B |= (1 << CS12);    // 256 prescaler 
+  TIMSK5 |= (1 << OCIE5A);  // enable timer compare interrupt
+  interrupts();             // enable all interrupts
+
+  Serial3.begin(1000000);
+  
+  fpga_next_buffer(Serial3);
+}
+
+volatile bool wait_for_timer = true;
+ISR(TIMER5_COMPA_vect) {
+  wait_for_timer = false;
+}
+
+void loop() {
+  if (!wait_for_timer) {
+    wait_for_timer = true;
+    // do at 60hz
+    
+    //gpu_draw_shape(Serial3, curShape.x, curShape.y, curShape.mask, curShape.color, COLOR_LIST);
+    //gpu_draw_board(Serial3, board, colors, COLOR_LIST);
+    fpga_next_buffer(Serial3);
+  }
+  //do continuously
+}
+
+void load_shape_to_gpu(){
+  
+}
+}
